9f2dc2b3cfda1eda23a54205376f7cf4090bc29e
bkorobkin <klayw1@gmail.com>
2024-03-14 18:07:07 +0100

    hw+lesson1_summary

---
+++ lesson1_hw1.js
@@ -0,0 +1,50 @@
+//TASK >>> Describe what result of the operation will be and specify it step by step  by comments:
+
+//'number' + 3 + 3
+console.log('number' + 3 + 3) //output: number33 - тому що number це (string), а оператор + якщо використовується з рядками, виконує конкатенацію (об'єднання), потiм number3 +3 робить теж саме.
+
+// 5 && "qwerty"
+console.log(5 && "qwerty") //output: qwerty - це прям квест був для мене, в нашому випадку выведе qwerty тому що 5 це true, якщо перше значення тру то выводить зразу друге(якщо коротко)
+
+// +'40' + +'2' + "hillel";
+console.log(+'40' + +'2' + "hillel") //output: 42hillel  - як i перший вираз тут конкатенацiя, але тому шо перед 40 ми ставимо + то цим ми варажаемо як число, тому послiдовно воно додає спочатку 2 а потiм об'єднує з string
+
+// '10' - 5 === 6;
+console.log('10' - 5 === 6) //output: false  - 10-5=5,  5!=6, тому false
+
+// true + false
+console.log(true + false) //output: 1 - з початку не зрозумiв, гуглив, це прям цiкаво)) все генiальне - просто, true це 1, false це 0, тож 1+0=1
+
+// '4px' - 3
+console.log('4px' - 3) //output: NaN - JS з оператором "-" пробує считати як число, але не може й видає NaN (not a number), nan - 3, тому що будь яке вираження з NaN буде Nan
+
+// '4' - 3
+console.log('4' - 3) //output: 1 - тут все просто, теж саме що зверху але без "px" JS вдало '4' iнтрепретує у число 4-3=1
+
+// '6' + 3 ** 0;
+console.log('6' + 3 ** 0) //output: 61 - тут я здивувався та пiшов до гуглу, тож ** оператор потужностi (Exponentiation) 3 в степени 0 = 1, '6'+1=61
+
+// 12 / '6'
+console.log(12 / '6') //output: 2 - якщо оператор '/' то JS '6' перетворює на число 
+
+// '10' + (5 === 6);
+console.log('10' + (5 === 6)) //output: 10false - з початку у скобках 5!=6 це false, 10+false=10false
+
+//>>>>>>>>>>>>?????????>>>>>>>> null == ''
+console.log(null == '') //output: false - но я не зрозумiв чому. null це 0 або ж false, пустий string це теж 0 або false, тож false==false має будти true. Нэ понятно=(
+// 3 ** (9 / 3);
+console.log(3 ** (9 / 3)) //output: 27 - 9-3=3, 3**3 або 3*3*3 = 27
+
+// !!'false' == !!'true'
+console.log(!!'false' == !!'true') //output: true - було складно, але здається зрозумiв. !! це дабл !, мы маємо двi cрiнги якi є тру, дабл !! робить спочатку з них False false а потом опять тру тру, тру==тру=тру
+
+// 0 || '0' && 1
+console.log(0 || '0' && 1) //output: 
+
+// (+null == false) < 1; 
+
+// false && true || true
+
+// false && (false || true);
+
+// (+null == false) < 1 ** 5;
---
+++ lesson1_summary.js
@@ -0,0 +1,31 @@
+//DATA TYPES / VARIABLES / Comparison and Logical Operators / 
+
+//      JavaScript has 8 Datatypes
+// 1. String - "text" or 'text'
+// 2. Number - 1, 2, 3, -1, -2, -3, 4.95, 0.99
+// 3. Bigint - "123456789012345678901234567890"
+// 4. Boolean - true or false / 1 or 0
+// 5. Undefined - a variable without a value
+// 6. Null - an empty or unknown value
+// 7. Symbol - represents a unique identifier (e.g.: && )
+// 8. Object - containers for named values called properties. (e.g.: const car = {type:"Fiat", model:"500", color:"white"};)
+
+//      JS Variables, can hold any type of data, can be declared in 4 ways:
+// Automatically
+// var - if you MUST support old browsers.
+// let - if you can't use const
+// const - if the value should not be changed or if the type should not be changed (Arrays and Objects)
+
+//      JS Comparison and Logical Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than	            (e.g.: x > 8	false)
+// <	less than	                (e.g.: x < 8	true)
+// >=	greater than or equal to	(e.g.: x >= 8	false)
+// <=	less than or equal to	    (e.g.: x <= 8	true)
+// &&	and	                        (e.g.: (x < 10 && y > 1) is true),   //false = 0 or null, true = anything else
+// ||	or	                        (e.g.: (x == 5 || y == 5) is false)
+// !	not	                        (e.g.: !(x == y) is true)
+// !!   double not, makes operator Not twice (e.g.: !!true is false and again is true)
