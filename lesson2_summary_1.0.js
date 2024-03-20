b7cdf9cf249e52e97dd226a307f390f1aa52f88f
bkorobkin <klayw1@gmail.com>
2024-03-20 17:26:33 +0100

    add js_homework3 + prompt

---
+++ lesson2_summary_1.0.js
@@ -0,0 +1,26 @@
+// JS Built-In functions, Js Collections and Loops
+
+// JS Built-In functions.
+// Loops.
+// Js Collections.
+// Links: https://pythontutor.com/
+
+// JS practice: https://www.codewars.com/dashboard
+
+// https://www.tutorialspoint.com/javascript/javascript_builtin_functions.htm
+
+// https://www.w3schools.com/js/js_number_methods.asp
+
+// https://www.w3schools.com/js/js_math.asp
+
+// https://www.w3schools.com/js/js_regexp.asp
+
+// https://uk.javascript.info/date
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// https://uk.javascript.info/array-methods
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
